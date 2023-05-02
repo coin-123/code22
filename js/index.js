6f906ec61ad08f337efefb755f9ad6a2d36b5fcb
MiracleChibuike <miraclechibuike27@gmail.com>
2023-05-02 20:57:19 +0100

    nav

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+function Icon(params) {
+    let toggle = document.querySelector(".navs");
+    if (toggle.style.display === "none") {
+        toggle.style.display = "block";
+    }else{
+        toggle.style.display = "none"
+    }
+}
